8d0662a3528835612391c4df59e420b121e68438
ying <huaiy1105@gmail.com>
2023-07-21 14:01:04 +0100

    add controller file, models, .FLASKENV

---
+++ controlloer/book_controller.py
@@ -0,0 +1,3 @@
+from flask import Flask, request, Blueprint
+from models.book import Book
+from models.book_list import books
---
+++ models/book_list.py
@@ -0,0 +1,10 @@
+from models.book import Book
+
+book1 = Book("My Brilliant Friend", "Elena Ferrante", "Fiction")
+book2 = Book("1Q84", "Haruki Murakami", "Fiction")
+book3 = Book("A little life", "Hanya Yanagihara", "Fiction")
+book4 = Book("Everything I Know About Love", "Dolly Alderton", "Self-help")
+book5 = Book("Fluent Python: Clear, Concise, and Effective Programming", "Luciano Ramalho", "Computer Science")
+book6 = Book("Clean Code: A Handbook of Agile Software Craftsmanship", "Robert C. Martin", "Computer Science")
+
+books = [book1, book2, book3, book4, book5, book6]
