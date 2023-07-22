afd184db876197a697597120194b3bff94ad7c03
ying <huaiy1105@gmail.com>
2023-07-22 13:32:55 +0100

    add remove book, check out a book

--- controller/book_controller.py
+++ controller/book_controller.py
@@ -16,13 +16,20 @@ def individual_book(index):
     return render_template("individual_book.jinja", title = books[int(index)].book_title, book = books[int(index)])
 
 # add new books
-@books_blueprint.route("/books", methods=["POST"])
+@books_blueprint.route("/books", methods=["POST", "GET"])
 def add_books():
     book_title = request.form["book_title"]
     author = request.form["author"]
     genre = request.form["genre"]
+    checked_out = request.form.get("checked_out")
 
-    new_books = Book(book_title, author, genre)
+    new_books = Book(book_title, author, genre, checked_out)
     books.append(new_books)
     # return render_template("index.jinja", title="The most borrowed books", book_list = books)
+    return redirect("/books")
+
+# remove a book from library
+@books_blueprint.route("/books/delete/<index>")
+def remove_book(index):
+    books.remove(books[int(index)])
     return redirect("/books")
--- models/book.py
+++ models/book.py
@@ -1,5 +1,6 @@
 class Book:
-    def __init__(self, book_title, author, genre):
+    def __init__(self, book_title, author, genre, checked_out):
         self.book_title = book_title
         self.author = author
         self.genre = genre
+        self.checked_out = checked_out
